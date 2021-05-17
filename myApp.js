21f5de6668897fd321bb75f653e994a1c7c0c1fb
matonton <>
2021-05-17 15:57:46 +0000

    added initial routing instructions

--- myApp.js
+++ myApp.js
@@ -1,24 +1,15 @@
 var express = require('express');
 var app = express();
 
+console.log("Hello World");
+console.log(__dirname);
 
+app.use('/public', express.static(__dirname + '/public'))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+  // res.send('Hello Express')
+  res.sendFile(__dirname + '/views/index.html')
+})
 
 
 
