e8c4087ad10d39e7a173820edb251023c2697367
matonton <>
2021-05-22 06:24:12 +0000

    added secret

--- myApp.js
+++ myApp.js
@@ -3,12 +3,19 @@ var app = express();
 
 console.log("Hello World");
 console.log(__dirname);
+console.log(process.env.MESSAGE_STYLE)
 
 app.use('/public', express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
   // res.send('Hello Express')
   res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/json', (req, res) => {
+  // res.send('Hello Express')
+  if (process.env.MESSAGE_STYLE == "uppercase") res.json({"message": "HELLO JSON"}) 
+  else res.json({"message": "Hello json"})
 })
 
 
